38227c298b2774d393fea25e3ec5a2a86cb94665
Suraj Prasad Soni <Surajsoni119@gmail.com>
2024-11-18 14:37:21 +0530

    Publishing application
    - publishing application

---
+++ README.md
@@ -0,0 +1,25 @@
+# Broadcast
+A app that will broadcast your message to all nearby users of  the app. <br>  You have to use pincode/zipcode of your area for using the app
+
+# Libraries used
+-> Coroutines
+<br>
+-> Retrofit
+<br>
+-> Gson
+<br>
+->Firebase Cloud Messaging
+<br>
+->Material UI
+<br>
+# App video
+https://drive.google.com/drive/folders/1kKXkFeb-dM5kQ_qlrAVTM2Ku-FnjgYCS?usp=sharing
+<br>
+<br>
+# Screenshots
+
+![scr5](https://user-images.githubusercontent.com/48099786/121035878-ef2e2780-c7cb-11eb-9463-76dccc024c02.jpeg)
+![scr1](https://user-images.githubusercontent.com/48099786/121035884-f05f5480-c7cb-11eb-92a1-6041db744c2d.jpeg)
+
+![scr3](https://user-images.githubusercontent.com/48099786/121035890-f0f7eb00-c7cb-11eb-89ef-ac4c62a5db2c.jpeg)
+![scr4](https://user-images.githubusercontent.com/48099786/121035892-f1908180-c7cb-11eb-8fc9-0e12f2778d84.jpeg)
---
+++ app/src/main/java/in/starbow/broadcast/MainActivity.kt
@@ -0,0 +1,56 @@
+package `in`.starbow.broadcast
+
+import `in`.starbow.broadcast.databinding.ActivityMainBinding
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.messaging.FirebaseMessaging
+import com.google.gson.Gson
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import java.lang.Exception
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        var pin: String? = intent.getStringExtra("pin")
+        Log.d("MAIN","$pin")
+        var Topic = "/topics/$pin"
+        FirebaseMessaging.getInstance().subscribeToTopic(Topic)
+        var broadCastBtn=findViewById<Button>(R.id.btnBroadCast)
+        var tit = findViewById<TextInputEditText>(R.id.edTitleTxt)
+        var msg = findViewById<TextInputEditText>(R.id.edtxtMsg)
+       broadCastBtn.setOnClickListener {
+           val title = tit.text.toString()
+           val message = msg.text.toString()
+           Log.d("DEB","$title"+" "+"$message");
+           if(title.isNotEmpty()&&message.isNotEmpty())
+           {
+                pushNotification(NotificationData(title, message), Topic).also {
+                    sendNotification(it)
+                }
+           }
+
+       }
+    }
+    private fun sendNotification(notification:pushNotification)= CoroutineScope(Dispatchers.IO).launch {
+        try{
+           val response = Retrofitinstance.api.postNotification(notification)
+        }catch (e:Exception){
+            Log.i("Main",e.toString())
+        }
+    }
+
+
+
+}
